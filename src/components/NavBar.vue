5a7b1fca4806fbf650b90a977c2f3ba1df4f0e47
EmptyRamenBox <alexis.martinezo92@gmail.com>
2020-04-12 02:08:42 -0500

    Added Navigation Drawer

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,11 +3,31 @@
     <!-- Drawer -->
     <!-- -->
     <!--  -->
-
+    <v-navigation-drawer app clipped v-model="drawer">
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.text" link>
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-subheader class="mt-4 grey--text text--darken-1">FRIENDS</v-subheader>
+        <v-list>
+          <v-list-item v-for="item in items2" :key="item.text" link>
+            <v-list-item-avatar>
+              <img :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`" alt />
+            </v-list-item-avatar>
+            <v-list-item-title v-text="item.text" />
+          </v-list-item>
+        </v-list>
+      </v-list>
+    </v-navigation-drawer>
     <!-- NavBar -->
     <!--  -->
     <!--  -->
-    <v-app-bar app color="primary" class="font-italic headline" dark>
+    <v-app-bar clipped-left dense app color="primary" class="font-italic headline" dark>
       <!-- Left Side -->
       <!--  -->
       <!--  -->
